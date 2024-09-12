ddc3726adb2c26119b62f2d5ce6f812966b08481
Claudio Perroni <c.perroni@ifortech.com>
2024-09-12 14:49:50 +0200

    wireui

---
+++ app/Models/Note.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'title', 
+        'body',
+        'send_date',
+        'is_published',
+        'heart_counter'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
