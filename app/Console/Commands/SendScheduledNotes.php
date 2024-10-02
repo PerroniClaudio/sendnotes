16fe40c071890e6b301de16f8c47cece491037c3
Claudio Perroni <c.perroni@ifortech.com>
2024-10-02 10:03:15 +0200

    invio mail

--- app/Console/Commands/SendScheduledNotes.php
+++ app/Console/Commands/SendScheduledNotes.php
@@ -2,6 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\SendEmail;
+use App\Models\Note;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class SendScheduledNotes extends Command
@@ -26,5 +29,22 @@ class SendScheduledNotes extends Command
     public function handle()
     {
         //
+
+        $now = Carbon::now();
+
+        $notes = Note::where('is_published', 1)
+            ->where('send_date', '<=' ,$now->toDateTimeString())
+            ->get();
+
+        $noteCount = $notes->count();
+
+        $this->info("Current time: {$now->toDateString()}.");
+        $this->info("Found {$noteCount} notes to send.");
+
+        foreach($notes as $note) {
+            $this->info("Sending note {$note->id} to {$note->recipient}.");
+
+            SendEmail::dispatch($note);
+        }
     }
 }
